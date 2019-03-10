7898531719a05c4811ec64e1df5ea81326a7f6bd
Simone Baracchi <simonebaracchi@gmail.com>
2019-03-10 22:07:31 +0100

    Cleanup

--- ssoret.py
+++ ssoret.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import networkx as nx
 from SpaghettiDistance import SpaghettiDistance
 import fileinput
@@ -39,16 +41,6 @@ for node in nodes:
         weight = calculator.get_similarity(node, othernode)
         G.add_edge(node, othernode, weight=weight)
 
-def get_weight(graph, path):
-    old = None
-    weight = 0
-    for node in path:
-        if old is None:
-            old = node
-            continue
-        weight += graph[old][node]['weight']
-    return weight
-
 def minus(list, item):
     copy = list[:]
     copy.remove(item)
@@ -69,16 +61,12 @@ def search_rec(graph, outer, remaining):
         totry = sorted(remaining, key=lambda x: graph.get_edge_data(outer, x)['weight'], reverse=True)[:depth]
         if len(totry) > 1 and graph.get_edge_data(outer, totry[0])['weight'] == graph.get_edge_data(outer, totry[1])['weight']:
             totry = [totry[0]]
-        #print('from {}, next nodes'.format(outer))
-        #for x in totry:
-        #    print('{} score {}'.format(x, graph.get_edge_data(outer, x)['weight']))
 
     for node in totry:
         inner_score, inner_path = search_rec(graph, node, minus(remaining, node))
 
         path = ([outer] + inner_path) if outer is not None else inner_path
         score = inner_score + (graph.get_edge_data(outer, node)['weight'] if outer is not None else 0)
-        #print('out {} rem  {} path {} score {}'.format(str(outer), str([str(x) for x in remaining]), str([str(x) for x in path]), score))
 
         if score > best_score:
             best_score = score
@@ -87,24 +75,5 @@ def search_rec(graph, outer, remaining):
 
 score, path = search_rec(G, None, nodes)
 
-#all_paths = nx.all_simple_paths(G, first_node, last_node)
-#heaviest_path = max((path for path in all_paths),
-#                    key=lambda path: get_weight(G, path))
-
-#best_source = None
-#best_target = None
-#best_score = 100000000
-#
-#for node in nodes: 
-#    for othernode in nodes: 
-#        if node == othernode:
-#            continue
-#
-#        score = nx.shortest_path_length(G, node, othernode, weight='weight')
-#        if score < best_score:
-#            best_source = node
-#            best_target = othernode
-#
-#path = nx.shortest_path(G, best_source, best_target, weight='weight')
 for node in path:
     print(str(node))
